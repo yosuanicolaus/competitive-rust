429a11c2707029ce4ca8ee1694571e59cbc86842
Yosua Nicolaus <yoni@odoo.com>
2024-12-03 22:55:03 +0100

    solve aoc 241203

---
+++ archive/2024/12/03.12.2024 - Advent of Code 241203/advent-of-code-241203-a.rs
@@ -0,0 +1,65 @@
+//{"name":"advent-of-code-241203-a","group":"Advent of Code 241203","url":"","interactive":false,"timeLimit":2000,"tests":[{"input":"","output":""},{"input":"","output":""}],"testType":"single","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"advent-of-code-241203-a"}}}
+
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+use algo_lib::misc::test_type::TaskType;
+use algo_lib::misc::test_type::TestType;
+use regex::Regex;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
+    let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    let mut res = 0;
+
+    while !input.is_empty() {
+        let hay = input.read_line();
+
+        for mul_text in re.find_iter(&hay).map(|m| m.as_str()) {
+            let (n1, n2) = mul_text[4..mul_text.len() - 1]
+                .split_once(',')
+                .map(|(a, b)| (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()))
+                .unwrap();
+            res += n1 * n2;
+        }
+    }
+
+    out.print_line(res);
+}
+
+pub static TEST_TYPE: TestType = TestType::Single;
+pub static TASK_TYPE: TaskType = TaskType::Classic;
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    let mut pre_calc = ();
+
+    match TEST_TYPE {
+        TestType::Single => solve(&mut input, &mut output, 1, &mut pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 1..=t {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &mut pre_calc);
+                i += 1;
+            }
+        }
+    }
+    output.flush();
+    match TASK_TYPE {
+        TaskType::Classic => input.is_empty(),
+        TaskType::Interactive => true,
+    }
+}
+
+//START MAIN
+mod tester;
+
+fn main() {
+    tester::run_tests();
+}
+//END MAIN
